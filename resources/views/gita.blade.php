8fa0d9cad7d8114fca8d183a633bb8c75b8a041b
ArifHidayatulloh <hidayatulloharif37@gmail.com>
2023-07-25 16:57:04 +0700

    mengubah bg gita

--- resources/views/gita.blade.php
+++ resources/views/gita.blade.php
@@ -1,1 +1,13 @@
-gita
+<html>
+    <style>
+        .container{
+            background-color: red
+        }
+    </style>
+    <body>
+        <div class="container">
+            gita
+        </div>
+
+    </body>
+</html>
